49b75d9fad60406f6c29a4c79cb90d96f4b22e1e
enridaga <enricodaga@gmail.com>
2020-09-23 10:31:27 +0100

    Upgraded to 0.6.9

--- apif/build.sh
+++ apif/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=${1:-v0.6.8}
+version=${1:-v0.6.9}
 docker build -t enridaga/spice-datahub-apif:$version --build-arg APIF_VERSION=$version -f Dockerfile .
--- apif/config-local.php
+++ apif/config-local.php
@@ -5,5 +5,13 @@ return [
         'host' => 'spice-datahub-mongodb',
         'port' => '27017',
         'database' => 'datahub',
+		'queryLimit' => 100,
+        'adminUser' => 'admin',
+        'adminPwd' => 'MONGO1234567890'
     ],
+    //Specify the dataset and key used for activity logging here
+    'activityLog' => [
+        'enabled' => true,
+        'dataset' => 'activity_log'
+    ]
 ];
